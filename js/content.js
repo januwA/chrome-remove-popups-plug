d0ee718bc24e937377471425baff8344a7abec47
januwA <ajanuw1641845087@sina.com>
2019-05-29 00:03:54 +0800

    储存设置

--- js/content.js
+++ js/content.js
@@ -1,9 +1,11 @@
 // 标签页中的内容脚本
 let max_zIndex = 10000;
-let setIntervalDelay = 1000;
+let setIntervalDelay = 10000;
+let setIntervalCtrl;
 let zIndexOverflowHistory = [];
+let pageAds = []; // 页面被隐藏的广告
 const constTypes = {
-  static: "static",
+  static: 'static',
 };
 class HistoryElement {
   constructor(el, oldDisplay) {
@@ -12,56 +14,76 @@ class HistoryElement {
   }
 
   update() {
-    this.el.style["display"] = this.oldDisplay;
+    this.el.style['display'] = this.oldDisplay;
   }
 }
 
 function removePopUpsEvent() {
-  let allElement = document.body.querySelectorAll("*");
+  let allElement = document.body.querySelectorAll('*');
   // 遍历页面上所有的元素
   for (let i = 0; i < allElement.length; i++) {
-    const element = allElement[i];
-    const position = getPV(element, "position");
-    const zIndex = getPV(element, "z-index");
-    const isFlter =
-      zIndex && Number.isInteger(Number(zIndex)) && zIndex >= max_zIndex;
-    if (position !== constTypes.static && isFlter) {
-      const oldDisplay = getPV(element, "display");
-      zIndexOverflowHistory.push(new HistoryElement(element, oldDisplay));
-      element.style["display"] = "none";
+    const el = allElement[i];
+    const position = getPV(el, 'position');
+    const zIndex = getPV(el, 'z-index');
+    if (
+      position === constTypes.static ||
+      !Number.isFinite(Number(zIndex)) ||
+      !zIndex ||
+      zIndex < max_zIndex
+    ) {
+      continue;
     }
+    const oldDisplay = getPV(el, 'display');
+    zIndexOverflowHistory.push(new HistoryElement(el, oldDisplay));
+    el.style['display'] = 'none';
   }
 }
 
-removePopUpsEvent();
-setInterval(removePopUpsEvent, setIntervalDelay);
+function startRemovePopUpsEvent() {
+  removePopUpsEvent();
+  setIntervalCtrl = setInterval(removePopUpsEvent, setIntervalDelay);
+}
+startRemovePopUpsEvent();
 
 function getPV(el, prop) {
   return document.defaultView.getComputedStyle(el, null).getPropertyValue(prop);
 }
 
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(msg, sender, sendResponse) {
-  max_zIndex = msg.data;
-  setIntervalDelay = Number(msg.delay);
-  for (const el of zIndexOverflowHistory) {
-    el.update();
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.zIndex && msg.delay) {
+    clearInterval(setIntervalCtrl);
+    setIntervalCtrl = null;
+    max_zIndex = Number(msg.zIndex);
+    setIntervalDelay = Number(msg.delay);
+
+    for (const el of zIndexOverflowHistory) el.update();
+    zIndexOverflowHistory = [];
+    startRemovePopUpsEvent();
   }
-  removePopUpsEvent();
-}
+
+  if (msg.isDelAds) {
+    hidePageAds();
+  } else {
+    for (const el of pageAds) {
+      el.update();
+    }
+  }
+});
 
 // 向扩展脚本发送消息 popup.js
 // 只有打开了pupup页面，才能收到消息
 // chrome.runtime.sendMessage(chrome.runtime.id, { data: max_zIndex });
 
-
 // 尽量隐藏掉页面上的广告.
 function hidePageAds() {
+  pageAds = [];
   document.querySelectorAll('*').forEach(el => {
     let cl = Array.from(el.classList);
-    if (cl.some(clItem => /ads/ig.test(clItem))) {
+    if (cl.some(clItem => /ads/gi.test(clItem))) {
+      const oldDisplay = getPV(el, 'display');
+      pageAds.push(new HistoryElement(el, oldDisplay));
       el.style['display'] = 'none';
     }
   });
-};
+}
 hidePageAds();
--- js/popup.js
+++ js/popup.js
@@ -1,39 +1,59 @@
-const btn = document.querySelector("#js-ok-btn");
-const input = document.querySelector("#js-input-zIndex");
-const inputDelay = document.querySelector("#js-input-delay");
+const qs = s => document.querySelector(s);
+const localStorage = window.localStorage;
+const btn = qs('#js-ok-btn');
+const input = qs('#js-input-zIndex');
+const inputDelay = qs('#js-input-delay');
+const adsCtrl = qs('#adsCtrl');
 
-// click buttin
-btn.addEventListener("click", function(e) {
-  handleMessageEvent();
-});
+window.addEventListener('load', e => {
+  const zIndex = localStorage.getItem('zIndex');
+  const delay = localStorage.getItem('delay');
+  const isDelAds = localStorage.getItem('isDelAds');
+  input.value = !!zIndex ? Number(zIndex) : 10000;
+  inputDelay.value = !!delay ? Number(delay) : 1000;
 
-// click Enter
-input.addEventListener("keydown", e => {
-  if (e.keyCode === 13) {
-    handleMessageEvent();
+  if (isDelAds && isDelAds === 'true') {
+    adsCtrl.setAttribute('checked', true);
+  } else {
+    adsCtrl.removeAttribute('checked');
   }
 });
 
+// click buttin
+btn.addEventListener('click', handleMessageEvent);
+
 function handleMessageEvent() {
-  let text = input.value;
+  let zIndex = input.value;
   let delay = Number(inputDelay.value);
-  if (text && delay >= 0) {
-    text = Math.round(Number(text));
-    const cfg = {
-      active: true,
-      currentWindow: true,
-    };
-    chrome.tabs.query(cfg, tabs => {
-      // 向内容脚本发送消息 content.js
-      chrome.tabs.sendMessage(tabs[0].id, {
-        data: text,
-        delay,
-      });
+  if (zIndex && delay >= 0) {
+    zIndex = Math.round(Number(zIndex));
+    localStorage.setItem('zIndex', zIndex);
+    localStorage.setItem('delay', delay);
+    sendMessage({
+      zIndex: zIndex,
+      delay,
     });
   }
 }
 
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(msg, sender, sendResponse) {
-  input.value = msg.data;
+adsCtrl.addEventListener('change', e => {
+  const message = {
+    isDelAds: adsCtrl.checked,
+  };
+  localStorage.setItem('isDelAds', adsCtrl.checked);
+  sendMessage(message);
+});
+
+function sendMessage(msgObj, cb = null) {
+  const cfg = {
+    active: true,
+    currentWindow: true,
+  };
+  chrome.tabs.query(cfg, tabs => {
+    // 向内容脚本发送消息 content.js
+    chrome.tabs.sendMessage(tabs[0].id, msgObj, null, cb);
+  });
 }
+// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+//   console.log(msg);
+// });
